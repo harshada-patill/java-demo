e4b18826a354a0b38465cc8b8d18289faa3c1871
harshada-patill <harshadapatil8725@gmail.com>
2025-12-09 19:09:15 +0530

    Object Initialization through Reference Variable

--- main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("harshada patil");
-        System.out.println("patil");
-    }
-}
